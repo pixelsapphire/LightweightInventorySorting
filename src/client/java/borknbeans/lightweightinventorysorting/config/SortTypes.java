d1f5c72a9fa8fb175e1d3131aed9adb997437014
pixelsapphire <35204889+pixelsapphire@users.noreply.github.com>
2025-02-23 21:03:26 +0100

    Add default ID sorting type

--- src/client/java/borknbeans/lightweightinventorysorting/config/SortTypes.java
+++ src/client/java/borknbeans/lightweightinventorysorting/config/SortTypes.java
@@ -1,24 +1,23 @@
 package borknbeans.lightweightinventorysorting.config;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 public enum SortTypes {
     ALPHANUMERIC,
     REVERSE_ALPHANUMERIC,
+    DEFAULT_ID,
     RAW_ID;
 
     public int compare(ItemStack left, ItemStack right) {
-        switch (this) {
-            case ALPHANUMERIC:
-                return alphanumeric(left, right);
-            case REVERSE_ALPHANUMERIC:
-                return reverseAlphanumeric(left, right);
-			case RAW_ID:
-                return rawId(left, right);
-        }
-
-        return 0;
+        return switch (this) {
+            case ALPHANUMERIC -> alphanumeric(left, right);
+            case REVERSE_ALPHANUMERIC -> reverseAlphanumeric(left, right);
+            case DEFAULT_ID -> defaultId(left, right);
+            case RAW_ID -> rawId(left, right);
+        };
     }
 
     private int alphanumeric(ItemStack left, ItemStack right) {
@@ -39,6 +38,17 @@ public enum SortTypes {
         }
 
         return (left.getName().getString().compareTo(right.getName().getString())) * -1;
+    }
+
+    private int defaultId(ItemStack left, ItemStack right) {
+        if (left.isEmpty() && !right.isEmpty()) {
+            return 0;
+        } else if (right.isEmpty() && !left.isEmpty()) {
+            return -1;
+        }
+
+        final Text leftName = left.getOrDefault(DataComponentTypes.ITEM_NAME, null), rightName = right.getOrDefault(DataComponentTypes.ITEM_NAME, null);
+        return (leftName == null ? "" : leftName.getString()).compareTo(rightName == null ? "" : rightName.getString());
     }
 
     private int rawId(ItemStack left, ItemStack right) {
